871e180034b9b6986d182f1f1305ad15f8e82fcb
Zhenhua <zhenhua1109@yahoo.com>
2022-07-19 17:00:00 -0700

    complete giphy app

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-// get the value of the search term
+'use strict';
 
-// call the api with the proper parameters
+// get the value of the search input
+
+// call the api with the proper parameters object
+
+// append the data link to the DOM
+
+// API KEY from Giphy
 const API_KEY = '47SoFht1UKvCbTBankcrDzcTocRDu5Xr';
-// append the dat to the dom
 
-async function getGiphyData(e) {
-  e.preventDefault();
+/** Initialize the app */
+function initApp() {
+
+  /** add event listener submit button*/
+  $('form').on('submit', getGiphyData);
+
+  /** add event listener on delete button */
+  $('#delete').on('click', () => {
+    $('.giffs').html('');
+  })
+}
+
+/** Given a giphy data URL, appends the dom with the gif */
+function updateDOM(giphyData) {
+  const imageLink = giphyData.data[0].images.original.url;
+  $('.giffs').append(`<img src = ${imageLink}>`);
+}
+
+/** When invoked, calls the giphy API and returns the URL for the gif */
+async function getGiphyData(evt) {
+  evt.preventDefault();
   const searchTerm = $('input').val();
 
   const parameters = { params: { api_key: API_KEY, q: searchTerm, limit: 1 } };
 
   const response = await axios.get('https://api.giphy.com/v1/gifs/search', parameters);
-  const imageSource = response.data.data[0];
-  console.log(imageSource.embed_url);
-  const $img = $('<img>');
-  $img.attr('src', imageSource.embed_url);
-  $img.attr({ width: '100px', height: "100px" });
-  $('.giffs').append($img);
 
-
+  updateDOM(response.data);
 }
-/** add event listener */
-$('form').on('submit', getGiphyData);
 
+initApp();
